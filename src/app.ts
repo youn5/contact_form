fff97bc510c5e6c096fcd61a5089a2e6e56c8f3f
Youn5 <etud8@yahoo.fr>
2023-12-14 14:40:17 +0100

    add nodemon

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('bonjour');
+import * as express from 'express';
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Hugo!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
