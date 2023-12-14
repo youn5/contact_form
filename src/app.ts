c7b9efcebf57b2b911782394e28ed5ea95626aa1
Youn5 <etud8@yahoo.fr>
2023-12-14 14:46:40 +0100

    add home

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,11 @@ import * as express from 'express';
 const app = express()
 const port = 3000
 
+app.set('view engine', 'pug')
+app.set('views', './src/views/')
+
 app.get('/', (req, res) => {
-  res.send('Hello Hugo!')
+  res.render('home', {title: 'Home', message: 'home page'});
 })
 
 app.listen(port, () => {
