ffee751ecff6ecad314ef5842ee89023990a834d
Youn5 <etud8@yahoo.fr>
2023-12-19 16:46:47 +0100

    fin

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # contact_form
+ Ce projet est un simple formulaire de contact construit avec Express.js et Pug, et est conçu pour être déployé avec Docker.
+
+## Installation
+
+1. Clonez ce dépôt sur votre machine locale.
+2. Naviguez vers le répertoire du projet dans votre terminal.
+3. Exécutez `docker-compose up --build` pour construire les images Docker pour le projet.
+
+## Utilisation
+
+1. après Exécuter `docker-compose up --build` les services Docker, y compris le serveur web, la base de données PostgreSQL et Adminer seront démarrés.
+2. Ouvrez votre navigateur et accédez à `http://localhost:3000` pour voir le formulaire de contact.
+3. Accédez à `http://localhost:8080` pour voir l'interface Adminer pour la base de données PostgreSQL.
+
+## Fonctionnalités
+
+- Validation des entrées du formulaire avec des expressions régulières.
+- Affichage des messages d'erreur en rouge au-dessus du formulaire.
+- Déploiement avec Docker et Docker Compose.
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,37 @@
 import * as express from 'express';
- 
 import * as bodyParser from 'body-parser';
 import { MessageSendSave } from './save';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set('view engine', 'pug')
+app.set('view engine', 'pug');
 
-app.set('views', './src/views/')
+app.set('views', './src/views/');
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+  console.log(`Server listening on port http://localhost:${port}`);
+});
 
 app.get('/', (req, res) => {
   res.render('home', { title: 'Home', message: 'home page' });
-})
+});
 
 app.get('/contact', (req, res) => {
-  res.render('contact', { title: 'contact ', });
-})
- 
-app.post('/submit-contact', (req, res) => {
-  console.log(req.body);
-  let error = false;
+  res.render('contact', { title: 'Contact' });
+});
+
+interface ErrorMessage {
+    surname?: string;
+    firstname?: string;
+    email?: string;
+    message?: string;
+}
+
+app.post('/submit-contact', async (req, res) => {
   const {
     User_Surname,
     User_Firstname,
@@ -35,53 +39,43 @@ app.post('/submit-contact', (req, res) => {
     User_Message,
   } = req.body;
 
-  // Utilisez des expressions régulières pour valider les entrées
   const nameRegex = /^[a-zA-Z]*$/;
   const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+  const errorMessages: ErrorMessage = {};
+
   if (!nameRegex.test(User_Surname)) {
-    res.send("Error: nom invalide ");
-    error = true;
-  }  
-  if (!nameRegex.test(User_Firstname)) {
-    res.send("Error: prenom invalide");
-    error = true;
-  }
-  if (!emailRegex.test(User_Email)) {
-    res.send("Error: Invalid email");
-    error = true;
-  }
-  if (User_Message.length < 10) {
-    res.send("Error: message doit avoir plus de 10 caractères");
-    error = true;
+    errorMessages.surname = 'Error: nom invalide';
   }
 
-  if (!error) {
-    let send = MessageSendSave.saveDatabase(User_Surname, User_Firstname, User_Email, User_Message);
-    console.log(send);
-    res.send("Message is saved successfully");
+  if (!nameRegex.test(User_Firstname)) {
+    errorMessages.firstname = 'Error: prenom invalide';
+  }
+
+  if (!emailRegex.test(User_Email)) {
+    errorMessages.email = 'Invalid email';
+  }
+
+  if (User_Message.length < 10) {
+    errorMessages.message = 'Error: message doit avoir plus de 10 caractères';
+  }
+
+  if (Object.keys(errorMessages).length > 0) {
+    res.render('contact', {
+      title: 'Contact',
+      errorMessages,
+      userSurname: User_Surname,
+      userFirstname: User_Firstname,
+      userEmail: User_Email,
+      userMessage: User_Message,
+    });
   } else {
-    res.send("Error: Invalid input");
+    await MessageSendSave.saveDatabase(User_Surname, User_Firstname, User_Email, User_Message);
+    res.render('contact', {
+      title: 'Contact',
+      successMessage: 'Message is saved successfully',
+    });
   }
 });
 
-// app.post('/submit-contact', (req, res) => {
-//   console.log(req.body)
-//   let error = false;
-//   const {
-//     User_Surname,
-//     User_Firstname,
-//     User_Email,
-//     User_Message,
-//   } = req.body;
-  
-//     let send = MessageSendSave.saveDatabase(User_Surname, User_Firstname, User_Email, User_Message);
-//   console.log(send);
-//   if (error) {
-//     res.send("Error")
-//   } else {
-//     res.send("Message is saved successfully")
-//   }
-// })
-
-
+ 
